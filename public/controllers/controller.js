785de306da905abf3fa36c4cb31c5d9f8d98a1b9
Maria Perez <mfperez13@gmail.com>
2017-04-26 20:45:30 -0700

    table done/todos and buttons displayed

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,53 @@
 const myApp = angular.module('todolistApp', []);
 
-myApp.controller('AppCtl', ['$scope'. '$http', function($scope, $http){
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
   console.log("Hello from Controller!");
-}]);
+
+  let refresh = function() {
+    // the '/todolist' is the route where we get out data from
+    $http.get('/todolist').then(function(response){
+      console.log("I got the data requested.");
+      $scope.todolist = response.data;
+    });
+  };
+  refresh();
+  /* Add todo function for button*/
+  $scope.addTodo = function(){
+    console.log($scope.todo); // checks that we received data from the input boxes
+    // $scope.todo is the data sent to the server
+    $http.post('/todolist', $scope.todo).then(function(response){
+      console.log(response);
+      refresh();
+    }); /*close of $http.post*/
+  }; /*close of $scope.addtodo*/
+
+  /* remove function for remove button*/
+  $scope.remove = function(id){
+    console.log(id);
+    $http.delete('/todolist/' + id).then(function(response){
+      refresh();
+    });
+  };  /*close of $scope.remove*/
+
+  /* edit function for edit button*/
+  $scope.edit = function(id) {
+    console.log(id);
+    $http.get('/todolist/' + id).then(function(response){
+        $scope.todo = response.data;
+    });
+  }; /*close of $scope.edit*/
+
+  /* update function for update button*/
+  $scope.update = function(){
+    console.log($scope.todo._id);
+    $http.put('/todolist/' + $scope.todo._id, $scope.todo).then(function(response){
+      refresh();
+    });
+  }; /*close of $scope.update*/
+
+  /* deselect function for clear button*/
+  $scope.deselect = function(){
+    $scope.todo = null;
+  }; /*close of $scope.deselect*/
+
+}]); // Close of myApp.controller
--- server.js
+++ server.js
@@ -1,10 +1,57 @@
 const express = require('express');
 const app = express();
+const mongojs = require('mongojs');
+const bodyParser = require('body-parser');
+
+// tells  you what database and collections will be used here
+const db = mongojs('todolist', ['todolist']);
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
-app.get('todolist', function(req, res){
+app.get('/todolist', function(req, res){
   console.log("I received a GET request.");
+
+  db.todolist.find(function(err, docs){
+    console.log(docs); // test to make sure we received the data
+    res.json(docs); // sends data back to the controller
+  });
+});
+
+// have server listen
+app.post('/todolist', function(req, res){
+  console.log(req.body); // prints the data received
+  db.todolist.insert(req.body, function(err, doc){
+    res.json(doc);
+  });
+});
+
+// delete from MongoDB database
+app.delete('/todolist/:id', function(req, res){
+  let id = req.params.id;
+  console.log(id);
+  db.todolist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+    res.json(doc);
+  });
+});
+
+app.get('/todolist/:id', function(req,res){
+  var id = req.params.id;
+  console.log(id);
+  db.todolist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+      res.json(doc);
+  });
+});
+
+// get todo by id and modify it
+app.put('/todolist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(req.body.name);
+    db.todolist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+      update: {$set: {title: req.body.title, text: req.body.text, due: req.body.due, status: req.body.status}},
+      new: true}, function(err, doc){
+        res.json(doc);
+    });
 });
 
 app.listen(3000);
